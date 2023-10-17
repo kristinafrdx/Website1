099a62ce0e5b09d56145701c6251de026b8ec185
kristinafrdx <kristina.frdx@mail.ru>
2023-10-17 23:46:15 +0400

    html, css

---
+++ main.js
@@ -0,0 +1,10 @@
+// (function () {
+//   const header = document.querySelector('.header');
+//   window.onscroll = () => {
+//     if (window.scrollY > 130) {
+//       header.classList.add('header_active');
+//     } else {
+//       header.classList.remove('header_active');
+//     }
+//   };
+// }());
